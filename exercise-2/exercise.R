37d80b909b9486af66976266a45d111ab62d96a7
hjy5@uw.edu <hjy5@uw.edu>
2016-10-25 15:03:53 -0700

    finished exercise.R

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,11 +8,24 @@ setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
+TopTenTracks <- function(artist) {
+  base <- 'https://api.spotify.com/v1/'
+  artist.query <- paste0(base, 'search?', 'q=', artist, '&type=artist')
+  artist.info <- fromJSON(artist.query)
+  artist.id <- artist.info$artists$items$id[1]
+  tracks.query <- paste0(base, 'artists/', artist.id, '/top-tracks?county=US')
+  top.tracks <- fromJSON(tracks.query)
+  return(top.tracks$tracks)
+  }
 
-
-
+base <- 'https://api.spotify.com/v1/'
+artist <- 'Drake'
+artist.query <- paste0(base, 'search?', 'q=', artist, '&type=artist')
+artist.info <- fromJSON(artist.query)
+names(artist.info)
+artist.id <- artist.info$artists$items$id[1]
 # What are the top 10 tracks by Nelly?
-
+TopTenNelly <- TopTenTracks('Nelly')
 
 
 
